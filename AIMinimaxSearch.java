a62689b07d45b71d2bedf72c60d1e5437cf5dc80
Chloe <chloeskillman@btinternet.com>
2019-09-26 12:26:45 +0100

    Update AIMinimaxSearch.java
    Updated code

--- AIMinimaxSearch.java
+++ AIMinimaxSearch.java
@@ -1,8 +1,6 @@
-package AIassignment;
-import java.util.*;
+package tictactoe;
 
-//Made by Chloe Skillman  reg no. - 1601341
-//Made by cs16523 
+import java.util.*;
 
 //aiplayer that will play best possible move in 3x3 Noughts&Crosses / Tictactoe
 //Human player is O
@@ -13,7 +11,7 @@ public class AIMinimaxSearch {
 List<PointsAndScores> ChildNodeSc;
 
 
-// variables used in Maximum
+//variables used in Maximum
 public int maxNumb;
 public int ord1;
 public int A;
@@ -28,82 +26,82 @@ public int minimaxMAX;
 
 protected int Maximum(List<Integer> list1) {   //return maximum                           
 	ord1 = -1;                                 //index variable, must equal -1
-    maxNumb = -99999;                          //max number, must equal extremely low number/ minimum number value
-    int i;  //variable made fore use in for loop
-    for (i = 0;list1.size() > i; i = 1 +i) {
-        if (maxNumb<list1.get(i) ) {
-        	ord1 = i;               
-            maxNumb = list1.get(i);  }
-    }
-    A = list1.get(ord1);
-    return A ;                                  //returns first maximum list
+  maxNumb = -99999;                          //max number, must equal extremely low number/ minimum number value
+  int i;  //variable made fore use in for loop
+  for (i = 0;list1.size() > i; i = 1 +i) {
+      if (maxNumb<list1.get(i) ) {
+      	ord1 = i;               
+          maxNumb = list1.get(i);  }
+  }
+  A = list1.get(ord1);
+  return A ;                                  //returns first maximum list
 }
 
 protected int Minimum(List<Integer> list2) {   //returns minimum
 	                                            
-    minNumb = 99999;                             //min number, must equal extremely big number/ maximum number value
-    ord2 = -1;                                   //index variable, must equal -1
-    
-    int j; //variable made fore use in for loop
-    for (j = 0; list2.size()> j; j = 1 +j) {    
-        if (minNumb > list2.get(j) ) {
-        	ord2 = j;
-            minNumb = list2.get(j);
-         }
-    }
-    B = list2.get(ord2);                          //returns minimum second list
-    return B;
+  minNumb = 99999;                             //min number, must equal extremely big number/ maximum number value
+  ord2 = -1;                                   //index variable, must equal -1
+  
+  int j; //variable made fore use in for loop
+  for (j = 0; list2.size()> j; j = 1 +j) {    
+      if (minNumb > list2.get(j) ) {
+      	ord2 = j;
+          minNumb = list2.get(j);
+       }
+  }
+  B = list2.get(ord2);                          //returns minimum second list
+  return B;
 }
 
 
 public int miniSearch(int depthInt, int turnsInt) {                        
-    List<Integer>scoreList = new ArrayList<>();
-    if (TicTacToe.b.hasOWon()){          // minimax search - if we(human player) win we get a lower number
-    	return -5; }
-    if (TicTacToe.b.hasXWon()){           
-    	return +5; }
-    
-    int scoreAtInstant;
-    Point pos;
-    
-    
-    List<Point> posList = TicTacToe.b.getAvailablePoints();   
-    if (posList.isEmpty()){ 
-    	return 1; }
-    
-    int u; 
-    for (u=0;posList.size() > u; u =1+u)
-    {   pos = posList.get(u); 
-        if (turnsInt != 1)                      //human player turn will pick lowest possible
-        	{ TicTacToe.b.placeMove(pos, 2);
-         scoreList.add(miniSearch(1 + depthInt, 1));    //going to the next depth/a depth deeper
+  List<Integer>scoreList = new ArrayList<>();
+  if (TicTacToe.b.hasOWon()){          // minimax search - if we(human player) win we get a lower number
+  	return -5; }
+  if (TicTacToe.b.hasXWon()){           
+  	return +5; }
   
-        } 
-    else if (turnsInt == 1)                   //ai player turn will pick highest possible
-       	{  TicTacToe.b.placeMove(pos, 1);
-           scoreAtInstant = miniSearch(1 + depthInt, 2);  //going a depth deeper
-           scoreList.add(scoreAtInstant); 
-           
-           if (depthInt < 1) {                        //if not depth 1 or further 
-                ChildNodeSc.add(new PointsAndScores(scoreAtInstant, pos));  }
-        } 
-        TicTacToe.b.board[pos.x][pos.y]=0;
-    }
-    
-    
-    if (turnsInt != 1) {     //if turnsInt is 1 then Maximum  will be return if not then minimum is returned
- 	   turnsInt = Minimum(scoreList);   //this is the min or max part of the minimax search
-    
-     return Minimum(scoreList);  
-    }
-     else  { turnsInt = Maximum(scoreList);
-    return Maximum(scoreList); 
-     }
+  int scoreAtInstant;
+  Point pos;
+  
+  
+  List<Point> posList = TicTacToe.b.getAvailablePoints();   
+  if (posList.isEmpty()){ 
+  	return 1; }
+  
+  int u; 
+  for (u=0;posList.size() > u; u =1+u)
+  {   pos = posList.get(u); 
+      if (turnsInt != 1)                      //human player turn will pick lowest possible
+      	{ TicTacToe.b.placeMove(pos, 2);
+       scoreList.add(miniSearch(1 + depthInt, 1));    //going to the next depth/a depth deeper
+
+      } 
+  else if (turnsInt == 1)                   //ai player turn will pick highest possible
+     	{  TicTacToe.b.placeMove(pos, 1);
+         scoreAtInstant = miniSearch(1 + depthInt, 2);  //going a depth deeper
+         scoreList.add(scoreAtInstant); 
+         
+         if (depthInt < 1) {                        //if not depth 1 or further 
+              ChildNodeSc.add(new PointsAndScores(scoreAtInstant, pos));  }
+      } 
+      TicTacToe.b.board[pos.x][pos.y]=0;
+  }
+  
+  
+  if (turnsInt != 1) {     //if turnsInt is 1 then Maximum  will be return if not then minimum is returned
+	   turnsInt = Minimum(scoreList);   //this is the min or max part of the minimax search
+  
+   return Minimum(scoreList);  
+  }
+   else  { turnsInt = Maximum(scoreList);
+  return Maximum(scoreList); 
+   }
 }
 
 public void MinimaxSearch(int depthInt, int turnsInt) {    //what we will call in TicTacToe.java
-    ChildNodeSc = new ArrayList<>( );
-    miniSearch(depthInt, turnsInt);     }
+  ChildNodeSc = new ArrayList<>( );
+  miniSearch(depthInt, turnsInt);     }
 
 protected Point BestPosMove() {  //return the best possible move     //uses Point from Board.java
 	 minimaxMIN = -5;                     //needs to have a larger value than minimaxMAX for Minimax search to work
